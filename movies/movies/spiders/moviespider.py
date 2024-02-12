e14c1f885b77e64fa069fa4930c6d9c265d27d6d
branson8815 <branson8815@gmail.com>
2024-02-12 11:25:12 -0800

    fixed up some pipelines and item fields

--- movies/movies/spiders/moviespider.py
+++ movies/movies/spiders/moviespider.py
@@ -18,20 +18,47 @@ class TheaterSpider(scrapy.Spider):
             audience_score = movie.css(
                 "score-pairs-deprecated::attr(audiencescore)"
             ).get()
+            more_info_path = movie.css("a.js-tile-link::attr(href)").get()
+            if more_info_path:
+                # Construct absolute URL for the movie details page
+                full_url = f"https://www.rottentomatoes.com{more_info_path}"
+                yield scrapy.Request(
+                    full_url,
+                    callback=self.parse_additional_content,
+                    meta={
+                        "title": title,
+                        "audience_score": audience_score,
+                        "critics_score": critics_score,
+                    },
+                )
 
-            yield MoviesItem(
-                title=title.strip() if title else None,
-                audience_rating=audience_score.strip() if title else None,
-                critic_rating=critics_score.strip() if title else None,
-            )
+    def parse_additional_content(self, response):
+        """
+        Here is our callback function as long as there are more pages available to load
+        """
+        title = response.meta.get("title")
+        critics_score = response.meta.get("critics_score")
+        audience_score = response.meta.get("audience_score")
+        genre = response.css("span.genre::text").get()
+        release_date = response.css(
+            "span[data-qa=movie-info-item-value] time::text"
+        ).get()
 
-            # load_more = response.css(
-            #     "button[data-qa='dlp-load-more-button']::text"
-            # ).get()
+        yield MoviesItem(
+            title=title if title else None,
+            audience_rating=audience_score if audience_score else None,
+            critic_rating=critics_score if critics_score else None,
+            genre=genre,
+            release_date=release_date,
+        )
 
-            # if load_more != None:
-            #     response.url += "?page=2"
-            #     self.parse()
+        # load_more = response.css(
+        #     "button[data-qa='dlp-load-more-button']::text"
+        # ).get()
+
+        # if load_more != None:
+        #     response.url += "?page=2"
+        #     self.parse()
 
 
 class HomeMovieSpider(scrapy.Spider):
@@ -76,7 +103,7 @@ class HomeMovieSpider(scrapy.Spider):
             ).get()
 
             yield MoviesItem(
-                title=title.strip() if title else None,
-                audience_rating=audience_score.strip() if audience_score else None,
-                critic_rating=critics_score.strip() if critics_score else None,
+                title=title if title else None,
+                audience_rating=audience_score if audience_score else None,
+                critic_rating=critics_score if critics_score else None,
             )
