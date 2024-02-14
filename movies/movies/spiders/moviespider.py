8b7c29f72eac7845f797f7952827e18bc586dbf5
branson8815 <branson8815@gmail.com>
2024-02-14 15:35:50 -0800

    still working on pagination' git push origin main

--- movies/movies/spiders/moviespider.py
+++ movies/movies/spiders/moviespider.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.http import FormRequest
 from movies.items import MoviesItem
 
 
@@ -6,11 +7,13 @@ class TheaterSpider(scrapy.Spider):
     name = "theater_spider"
     allowed_domains = ["www.rottentomatoes.com"]
     start_urls = ["https://www.rottentomatoes.com/browse/movies_in_theaters/"]
+    page = 1
 
     def parse(self, response):
+        # Extract movie information from the current page
+
         movies = response.css(".js-tile-link")
         for movie in movies:
-
             title = movie.css("span.p--small::text").get()
             critics_score = movie.css(
                 "score-pairs-deprecated::attr(criticsscore)"
@@ -21,7 +24,7 @@ class TheaterSpider(scrapy.Spider):
             more_info_path = movie.css("a.js-tile-link::attr(href)").get()
             if more_info_path:
                 # Construct absolute URL for the movie details page
-                full_url = f"https://www.rottentomatoes.com{more_info_path}"
+                full_url = response.urljoin(more_info_path)
                 yield scrapy.Request(
                     full_url,
                     callback=self.parse_additional_content,
@@ -32,9 +35,19 @@ class TheaterSpider(scrapy.Spider):
                     },
                 )
 
+        # Find and follow the link to the next page
+        load_more_button = response.css('button[data-qa="dlp-load-more-button"]')
+        if self.page < 10:
+            if load_more_button:
+                self.page += 1
+                yield response.follow(
+                    f"https://www.rottentomatoes.com/browse/movies_in_theaters/?page={self.page}",
+                    callback=self.parse,
+                )
+
     def parse_additional_content(self, response):
         """
-        Here is our callback function as long as there are more pages available to load
+        Callback function to parse additional content from the movie details page
         """
         title = response.meta.get("title")
         critics_score = response.meta.get("critics_score")
@@ -51,14 +64,6 @@ class TheaterSpider(scrapy.Spider):
             genre=genre,
             release_date=release_date,
         )
-
-        # load_more = response.css(
-        #     "button[data-qa='dlp-load-more-button']::text"
-        # ).get()
-
-        # if load_more != None:
-        #     response.url += "?page=2"
-        #     self.parse()
 
 
 class HomeMovieSpider(scrapy.Spider):
